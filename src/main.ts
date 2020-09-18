51db1cb700e57e5bc834ddf597d3b2306151f4b9
sebamomann <github@sebamomann.de>
2020-09-18 21:36:45 +0200

    cors ...

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 
+const cors = require('cors');
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    app.use(function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, POST, PUT, DELETE');
-        res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization, X-Auth-Token, content-type');
-        return next();
-    });
+    app.use(cors());
+    // app.use(function (req, res, next) {
+    //     res.setHeader('Access-Control-Allow-Origin', '*');
+    //     res.header('Access-Control-Allow-Methods', 'GET, OPTIONS, POST, PUT, DELETE');
+    //     res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization, X-Auth-Token, content-type');
+    //     return next();
+    // });
 
     await app.listen(3000);
 }
